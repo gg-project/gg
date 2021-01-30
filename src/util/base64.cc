981c3114c4b829a356141254fb30da524581beee
Alex Ozdemir <aozdemir@hmc.edu>
2021-01-30 14:54:34 -0800

    Remove Cryptopp, CMake build system
    * Remove the cryptopp dependency
    * Move to CMake build system

--- src/util/base64.cc
+++ src/util/base64.cc
@@ -2,36 +2,56 @@
 
 #include "base64.hh"
 
-#include <crypto++/base64.h>
-#include <crypto++/hex.h>
-#include <crypto++/sha.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 using namespace gg;
-using namespace CryptoPP;
 
-string base64::encode( const string& input )
+#define ALPHABET                                                               \
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+
+// Modified from
+// https://stackoverflow.com/questions/180947/base64-decode-snippet-in-c
+string base64::encode( const string& in )
 {
-  SHA256 hash_function;
-  string ret;
 
-  /* Each stage of the Crypto++ pipeline will delete the pointer it owns
-     (https://www.cryptopp.com/wiki/Pipelining) */
+  string out;
 
-  StringSource s( input, true, new Base64Encoder( new StringSink( ret ) ) );
-
-  return ret;
+  int val = 0, valb = -6;
+  for ( uint8_t c : in ) {
+    val = ( val << 8 ) + c;
+    valb += 8;
+    while ( valb >= 0 ) {
+      // modified from original for URL
+      out.push_back( ALPHABET[( val >> valb ) & 0x3F] );
+      valb -= 6;
+    }
+  }
+  if ( valb > -6 )
+    out.push_back( ALPHABET[( ( val << 8 ) >> ( valb + 8 ) ) & 0x3F] );
+  return out;
 }
 
-string base64::decode( const string& input )
+string base64::decode( const string& in )
 {
-  SHA256 hash_function;
-  string ret;
 
-  /* Each stage of the Crypto++ pipeline will delete the pointer it owns
-     (https://www.cryptopp.com/wiki/Pipelining) */
+  string out;
 
-  StringSource s( input, true, new Base64Decoder( new StringSink( ret ) ) );
+  vector<int> T( 256, -1 );
+  for ( int i = 0; i < 64; i++ )
+    T[ALPHABET[i]] = i;
 
-  return ret;
+  int val = 0, valb = -8;
+  for ( uint8_t c : in ) {
+    if ( T[c] == -1 )
+      break;
+    val = ( val << 6 ) + T[c];
+    valb += 6;
+    if ( valb >= 0 ) {
+      out.push_back( char( ( val >> valb ) & 0xFF ) );
+      valb -= 8;
+    }
+  }
+  return out;
 }
--- src/util/digest.cc
+++ src/util/digest.cc
@@ -1,28 +1,23 @@
 /* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 
 #include "digest.hh"
+#include "base64.hh"
 
-#include <crypto++/base64.h>
-#include <crypto++/hex.h>
-#include <crypto++/sha.h>
+#include <iostream>
+#include <string>
+
+#include <openssl/sha.h>
 
 using namespace std;
 using namespace gg;
-using namespace CryptoPP;
 
 string digest::sha256( const string& input )
 {
-  SHA256 hash_function;
-  string ret;
-
-  /* Each stage of the Crypto++ pipeline will delete the pointer it owns
-     (https://www.cryptopp.com/wiki/Pipelining) */
-
-  StringSource s(
-    input,
-    true,
-    new HashFilter( hash_function,
-                    new Base64URLEncoder( new StringSink( ret ), false ) ) );
-
-  return ret;
+  unsigned char hash[SHA256_DIGEST_LENGTH];
+  SHA256_CTX sha256;
+  SHA256_Init( &sha256 );
+  SHA256_Update( &sha256, input.c_str(), input.length() );
+  SHA256_Final( hash, &sha256 );
+  string b64 = base64::encode( string( hash, hash + SHA256_DIGEST_LENGTH ) );
+  return b64;
 }
