d7f3efd31595610ebade6f32fd29f294e9c53ec9
Alex Ozdemir <aozdemir@hmc.edu>
2021-03-11 23:09:26 -0800

    Fix byte order of base16

--- src/util/base16.cc
+++ src/util/base16.cc
@@ -16,8 +16,8 @@ string base16::encode( const string& in )
 
   string out;
   for ( uint8_t c : in ) {
-    out.push_back(ALPHABET[(c & 0x0F)]);
     out.push_back(ALPHABET[((c >> 4) & 0x0F)]);
+    out.push_back(ALPHABET[(c & 0x0F)]);
   }
   return out;
 }
